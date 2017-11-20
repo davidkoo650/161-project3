c9afde83fb80894294402aae958029b77bf27eca
DaeWoonKoo <davidkoo650@berkeley.edu>
2017-11-19 19:42:16 -0800

    proj3

--- project3/common.py
+++ project3/common.py
@@ -211,7 +211,7 @@ class PacketUtils:
                 self.send_pkt(ttl = i, payload = triggerfetch, flags = "PA",
                           seq = sequence + 1, ack = y + 1, sport = source)
 
-            pkt = self.get_pkt()
+            pkt = self.get_pkt(1)
 
             rst_list.append(False)
             ip_list.append(None)
@@ -226,7 +226,6 @@ class PacketUtils:
                     ip_list[last_index] = pkt[IP].src
                     return ip_list, rst_list
 
-                pkt = self.get_pkt()
+                pkt = self.get_pkt(1)
 
         return ip_list, rst_list
-
