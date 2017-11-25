5c5fb481d34f82fb3cb55d9c141b33794bc2e68a
DaeWoonKoo <davidkoo650@berkeley.edu>
2017-11-25 11:48:23 -0800

    modify part3

--- project3/common.py
+++ project3/common.py
@@ -256,7 +256,7 @@ class PacketUtils:
                 self.send_pkt(ttl = i, payload = triggerfetch, flags = "PA",
                           seq = sequence + 1, ack = y + 1, sport = source)
 
-            pkt = self.get_pkt(2)
+            pkt = self.get_pkt(1)
 
             rst_list.append(False)
             ip_list.append(None)
@@ -268,7 +268,8 @@ class PacketUtils:
 
                 if isRST(pkt):
                     rst_list[last_index] = True
+                    return ip_list, rst_list
 
-                pkt = self.get_pkt(2)
+                pkt = self.get_pkt(1)
 
         return ip_list, rst_list
