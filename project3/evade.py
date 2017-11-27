aaf3b2d4c0d4e4f79c762133fe40c299a906de1b
DaeWoonKoo <davidkoo650@berkeley.edu>
2017-11-27 13:03:15 -0800

    part4 test

--- project3/evade.py
+++ project3/evade.py
@@ -5,7 +5,7 @@ import common
 
 if __name__ == '__main__':
     # www.miit.gov.cn                                                           
-    target = "202.106.121.6"
+    target = "34.224.169.21"
     msg = open("msg.txt").read()
     
     myip = None
